b99f031c0e9e0f2087420addf0cf62a509ecadbf
fhrancoo <132franco@gmail.com>
2019-04-15 06:10:16 -0300

    Primer Commit

---
+++ js/navegador.js
@@ -0,0 +1,9 @@
+function navFunction() {
+    var showNav = document.getElementById('showNav').classList.toggle('showNav');
+
+};
+
+function navCerrar() {
+    var showNav = document.getElementById('showNav').classList.remove('showNav');
+
+};
